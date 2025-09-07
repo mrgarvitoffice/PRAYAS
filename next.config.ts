5aebb220bd17ff09207444dd0f3618d5fe7ebfc6
QuentrixHive <quentrixhive@gmail.com>
2025-09-07 10:29:32 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -88,6 +88,12 @@ const nextConfig: NextConfig = {
         hostname: 'media.tegna-media.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform.mmafighting.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
