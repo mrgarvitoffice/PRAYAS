0e46ed951a6e25d6c61a3ed4395b9c583ed40909
QuentrixHive <quentrixhive@gmail.com>
2025-09-07 10:08:55 +0000

    Error: Invalid src prop (https://cdn.mos.cms.futurecdn.net/25dECvfqxNecs

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: '**.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.mos.cms.futurecdn.net',
         port: '',
         pathname: '/**',
       },
