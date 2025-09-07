989a2cc774ef4ad42c0bbc367c07bbadecfd3c05
QuentrixHive <quentrixhive@gmail.com>
2025-09-07 10:26:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'media.assettype.com',
         port: '',
         pathname: '/**',
@@ -68,6 +68,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'static.toiimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'th-i.thgim.com',
         port: '',
         pathname: '/**',
       },
